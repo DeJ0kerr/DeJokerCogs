2acd8e4aab7dca9ef89451ba788fd35041b6b658
erez9901 <erez9901@gmail.com>
2019-02-02 18:41:15 +0200

    Added repo info

--- LogEverything/LogEverything.py
+++ LogEverything/LogEverything.py
@@ -1,8 +1,8 @@
+from typing import Any
+
 import discord
 from redbot.core import commands
-from redbot.core import checks
 from redbot.core.bot import Red
-from typing import Any
 
 Cog: Any = getattr(commands, "Cog", object)
 
@@ -12,7 +12,7 @@ class LogEverything(Cog):
     def __init__(self, bot: Red):
         self.bot = bot
 
-    @commands.command("channelid")
+    @commands.command(name="channelid")
     async def channel_id_cmd(self, ctx: commands.Context):
         channel: discord.DMChannel = ctx.channel
         await channel.send(channel.id)
