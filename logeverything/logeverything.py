072e42048af5403dc426a4c8705ce1e4e120d0ca
erez9901 <erez9901@gmail.com>
2019-02-02 19:25:44 +0200

    first event

--- logeverything/logeverything.py
+++ logeverything/logeverything.py
@@ -17,4 +17,6 @@ class LogEverything(Cog):
         channel: discord.DMChannel = ctx.channel
         await channel.send(channel.id)
 
-    # def on_member_update(self, before, after):
+    def on_member_update(self, before: discord.Member, after: discord.Member):
+        channel: discord.DMChannel = self.bot.get_channel(id=520225411070689280)
+        await channel.send("before {} after {}".format(before.display_name, after.display_name))
